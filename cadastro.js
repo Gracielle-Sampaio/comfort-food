b531c70e69aabdbf03e10078f025c87c9626e670
GracielleSampaio <bysunshinearts@gmail.com>
2022-04-05 22:27:44 -0300

    Pagina de Cadastro
    feito com html, css e javascrip

---
+++ cadastro.js
@@ -0,0 +1,11 @@
+function confimarCadastro(){
+  let nomecompleto = document.getElementById('Fname')
+  let nomeusuario = document.getElementById('Uname')
+  let senha = document.getElementById('pass')
+
+  if ( (nomecompleto.value.length == 0) || (nomeusuario.value.length == 0) || (senha.value.length == 0)  ){
+    alert("[ERROR] Preencha todas os campos!")
+  } else{
+    alert("Cadastro Concluido com Sucesso!")
+  }
+}
